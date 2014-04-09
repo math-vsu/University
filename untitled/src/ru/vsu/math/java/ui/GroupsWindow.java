0ca1a3b42e1de29e2f5f5bece7739eaa184b81e6
Stanislav Mekhonoshin <ejabberd@gmail.com>
2014-04-09 21:16:19 +0400

    Заготовка для получения списка групп в окне

--- untitled/src/ru/vsu/math/java/ui/GroupsWindow.java
+++ untitled/src/ru/vsu/math/java/ui/GroupsWindow.java
@@ -1,13 +1,16 @@
 package ru.vsu.math.java.ui;
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 import java.awt.event.*;
+import ru.vsu.math.java.entity.Group;
 /**
  * Created by Дмитрий on 09.03.14.
  */
 public class GroupsWindow extends JFrame {
-    public GroupsWindow(){
+    public GroupsWindow(List list){
         super("Список групп");
         setBounds(100, 100, 700, 700);
+        List<Group> groupsList = list;
     }
 }
